335e4b19d38f5802792317722eaa90c8f041c0a1
Harun Demir <harun.demir918@outlook.com>
2023-06-28 23:33:58 +0300

    app bar fix

--- lib/core/widgets/responsive_app_bar/responsive_desktop_app_bar.dart
+++ lib/core/widgets/responsive_app_bar/responsive_desktop_app_bar.dart
@@ -6,6 +6,7 @@ Date: 28.06.2023
 import 'package:flutter/material.dart';
 
 import '../../constants/constants.dart';
+import '../../utils/size_utils.dart';
 import 'responsive_app_bar_actions_item.dart';
 import 'responsive_app_bar_title.dart';
 
@@ -14,16 +15,30 @@ class ResponsiveDesktopAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => AppBar(
-        title: const ResponsiveAppBarTitle(),
+        title: Padding(
+          padding: EdgeInsets.only(
+            left: SizeUtils.getDynamicWidth(context, 0.08),
+          ),
+          child: const ResponsiveAppBarTitle(),
+        ),
         backgroundColor: Colors.transparent,
         elevation: 0,
-        actions: pageList
-            .map(
-              (item) => ResponsiveAppBarActionsItem(
-                index: item.index,
-                title: item.title,
-              ),
-            )
-            .toList(),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(
+              right: SizeUtils.getDynamicWidth(context, 0.08),
+            ),
+            child: Row(
+              children: pageList
+                  .map(
+                    (item) => ResponsiveAppBarActionsItem(
+                      index: item.index,
+                      title: item.title,
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+        ],
       );
 }
